09e4c76fc71de4acb3f70c65306c0a292c0dc052
Peter Kasson <kasson@pmks03.med.virginia.edu>
2014-01-12 15:50:10 -0500

    clusteratoms.py will now write Gromacs index files

--- clusteratoms.py
+++ clusteratoms.py
@@ -10,33 +10,50 @@ import gflags
 import numpy
 from scipy.cluster import hierarchy
 
+import ndx
 import PDB
 
 
-def clusteratoms(PDBfilename, atomnamesel, nclusters):
+def clusteratoms(PDBfilename, atomnamesel, nclusters, writendx=None):
   """Cluster atoms into k groups by nearest neighbor.
     Args:
       PDBfilename:  input file
       atomnamesel: regular expression for atom names to select
       nclusters: number of clusters to make
+      writendx: optional filename of Gromacs index to write
     Rets:
       clusterdata: array where each row is atomid, resid, clusternum.
   """
   infile = open(PDBfilename, 'r')
   pdbrecord = PDB.readPDB(infile)
   atomlines = []
+  idxctr = 0
   for line in pdbrecord[0]:
     if line.__class__ in [PDB.ATOM, PDB.HETATM]:
-      if re.search(atomnamesel, line.name):
-        atomlines.append([line.serial, line.resSeq, line.x, line.y, line.z])
+      if re.match(atomnamesel, line.name):
+        atomlines.append([line.serial, line.resSeq, idxctr,
+                          line.x, line.y, line.z])
+    idxctr += 1
   atomarr = numpy.array(atomlines)
-  clusteridx = hierarchy.fclusterdata(atomarr[:, 2:4], nclusters,
+  # alternate approach:
+  # specify maxdist, then:
+  # clusteridx = hierarchy.fclusterdata(atomarr[:, 3:6], maxdist,
+  #                                     criterion='distance') 
+  clusteridx = hierarchy.fclusterdata(atomarr[:, 3:6], nclusters,
                                       criterion='maxclust')
   # slightly kludgy construction of return data structure
   clusterdata = numpy.zeros([len(clusteridx), 3])
   clusterdata[:, 0] = atomarr[:, 0]
   clusterdata[:, 1] = atomarr[:, 1]
   clusterdata[:, 2] = clusteridx
+  # option to write a Gromacs index
+  if writendx:
+    indexdict = {}
+    indexdict['IndexNames'] = ['Cluster%d' % i for i in range(nclusters)]
+    indexdict['IndexGroups'] = []
+    for i in range(nclusters):
+      indexdict['IndexGroups'].append(atomarr[numpy.nonzero(clusteridx == i+1)[0], 2])
+    ndx.write_index(indexdict, writendx)
   return clusterdata
 
 
@@ -48,8 +65,11 @@ if __name__ == '__main__':
                        'Regexp to select atom names')
   gflags.DEFINE_string('outfile', 'out.dat',
                        'Output file name')
+  gflags.DEFINE_string('ndxfile', None,
+                       'Optional output index')
   gflags.DEFINE_integer('numclusters', 2,
                        'Number of clusters to make')
   argv = FLAGS(sys.argv)
-  clusters = clusteratoms(FLAGS.infile, FLAGS.atomsel, FLAGS.numclusters)
+  clusters = clusteratoms(FLAGS.infile, FLAGS.atomsel, FLAGS.numclusters,
+                          FLAGS.ndxfile)
   numpy.savetxt(FLAGS.outfile, clusters, fmt='%d\t%d\t%d')
